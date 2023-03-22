b3bf18b85dd6aa31ce26f893d5e7587695afa568
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-22 17:11:52 +0100

    add: custom classes and styling for .box .fizz .buzz .fizzbuzz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
-const main = document.querySelector("main");
+const main = document.querySelector(".container");
 
 for(let i = 1; i <= 100; i++){
-  const span = document.createElement("h1");
+  const span = document.createElement("div");
+  span.classList.add("box")
 
   if(!(i % 3) && !(i % 5)){
-    span.innerHTML = "FizzBuzz" + " " + i;
+    span.classList.add("fizzbuzz");
+    span.innerHTML = "FizzBuzz";
     main.append(span);
   }else if(!(i % 3)){
-    span.innerHTML = "Fizz" + " " + i;
+    span.classList.add("fizz");
+    span.innerHTML = "Fizz";
     main.append(span);
   }else if(!(i % 5)){
-    span.innerHTML = "Buzz" + " " + i;
+    span.classList.add("buzz");
+    span.innerHTML = "Buzz";
     main.append(span);
   }else{
     span.innerHTML = i;

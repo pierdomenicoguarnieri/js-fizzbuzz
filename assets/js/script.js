598425170847743ef96b02c90176032b81ceaae9
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-22 16:52:59 +0100

    add: output in html using append()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-for(let i = 0; i <= 100; i++){
-  if(!(i % 3) && !(i % 5) && i){
-    console.log("FizzBuzz", i);
-  }else if(!(i % 3) && i){
-    console.log("Fizz", i)
-  }else if(!(i % 5) && i){
-    console.log("Buzz", i);
+const main = document.querySelector("main");
+
+for(let i = 1; i <= 100; i++){
+  const span = document.createElement("h1");
+
+  if(!(i % 3) && !(i % 5)){
+    span.innerHTML = "FizzBuzz" + " " + i;
+    main.append(span);
+  }else if(!(i % 3)){
+    span.innerHTML = "Fizz" + " " + i;
+    main.append(span);
+  }else if(!(i % 5)){
+    span.innerHTML = "Buzz" + " " + i;
+    main.append(span);
   }else{
-    console.log(i);
+    span.innerHTML = i;
+    main.append(span);
   }
 }

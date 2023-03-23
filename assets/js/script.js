2a2eb97dde1b2d7d9d4e464e69f006be4e4f544e
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-23 10:07:58 +0100

    changed: optimized javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,20 @@ const container = document.querySelector(".container");
 
 for(let i = 1; i <= 100; i++){
   const box = document.createElement("div");
-  box.classList.add("box")
+  box.classList.add("box");
+  let output = i;
 
   if(!(i % 3) && !(i % 5)){
     box.classList.add("fizzbuzz");
-    box.innerHTML = "FizzBuzz";
-    container.append(box);
+    output = "FizzBuzz"
   }else if(!(i % 3)){
     box.classList.add("fizz");
-    box.innerHTML = "Fizz";
-    container.append(box);
+    output = "Buzz";
   }else if(!(i % 5)){
     box.classList.add("buzz");
-    box.innerHTML = "Buzz";
-    container.append(box);
-  }else{
-    box.innerHTML = i;
-    container.append(box);
+    output = "Buzz";
   }
+
+  box.append(output);
+  container.append(box);
 }
